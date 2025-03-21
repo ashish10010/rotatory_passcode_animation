43c229b535162516345f446773861f7aad0eeeea
Ashish Kunwar <thaladipesh89@gmail.com>
2025-03-21 21:10:50 +0545

    err fix

--- lib/passcode_input_view.dart
+++ lib/passcode_input_view.dart
@@ -3,19 +3,20 @@ import 'package:local_hero/local_hero.dart';
 
 import 'widgets/widgets.dart';
 
-const _animationDuration = Duration(microseconds: 500);
+const _animationDuration = Duration(milliseconds: 500);
 const _padding = 16.0;
 
 class PasscodeInputView extends StatefulWidget {
-  final String expectedCode;
-  final VoidCallback onSuccess;
-  final VoidCallback onError;
   const PasscodeInputView({
     required this.expectedCode,
     required this.onSuccess,
     required this.onError,
     super.key,
   });
+
+  final String expectedCode;
+  final VoidCallback onSuccess;
+  final VoidCallback onError;
 
   @override
   State<PasscodeInputView> createState() => _PasscodeInputViewState();
@@ -24,11 +25,11 @@ class PasscodeInputView extends StatefulWidget {
 class _PasscodeInputViewState extends State<PasscodeInputView>
     with SingleTickerProviderStateMixin {
   late final AnimationController _modeChangeController;
-  late final List<PasscodeDigit> _passcodeDigitValues;
+
+  late List<PasscodeDigit> _passcodeDigitValues;
   var _currentInputIndex = 0;
 
   var _simpleInputMode = false;
-
   var _passcodeAnimationInProgress = false;
 
   bool get _isAnimating =>
@@ -37,26 +38,34 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
   @override
   void initState() {
     super.initState();
-    _modeChangeController = AnimationController(vsync: this)
-      ..addListener(() => setState(() {}));
+
+    _modeChangeController = AnimationController(
+      duration: _animationDuration * 2,
+      vsync: this,
+    )..addListener(() => setState(() {}));
+
     _resetDigits();
   }
 
   @override
   void dispose() {
     _modeChangeController.dispose();
+
     super.dispose();
   }
 
-  void _onDigitSelected(int index, {bool autoValidate = false}) {
+  void _onDigitSelected(int index, {bool autovalidate = false}) {
     if (_isAnimating) return;
+
     final digitValue = _passcodeDigitValues[_currentInputIndex];
+
     setState(() {
       _passcodeDigitValues[_currentInputIndex++] = digitValue.copyWith(
         value: RotaryDialConstants.inputValues[index],
       );
     });
-    if (autoValidate) _validatePasscode();
+
+    if (autovalidate) _validatePasscode();
   }
 
   void _resetDigits() => setState(() {
@@ -71,6 +80,7 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
 
   Future<void> _validatePasscode() async {
     if (_isAnimating) return;
+
     final expectedCode = widget.expectedCode;
 
     if (_currentInputIndex != expectedCode.length) return;
@@ -80,13 +90,16 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
       '',
       (code, element) => code += element.value?.toString() ?? '',
     );
+
     _togglePasscodeAnimation();
+
     if (codeInput == expectedCode) {
       await _changePasscodeDigitColors(
         backgroundColor: Colors.green,
         fontColor: Colors.transparent,
         interval: interval,
       );
+
       widget.onSuccess();
     } else {
       await _changePasscodeDigitColors(
@@ -100,8 +113,10 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
         fontColor: Colors.white,
         interval: interval,
       );
+
       widget.onError();
     }
+
     await Future.delayed(_animationDuration);
     _resetDigits();
     _togglePasscodeAnimation();
@@ -113,7 +128,7 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
     int interval = 0,
   }) async {
     for (var i = 0; i < _passcodeDigitValues.length; i++) {
-      await Future.delayed(Duration(microseconds: interval));
+      await Future.delayed(Duration(milliseconds: interval));
 
       setState(() {
         if (backgroundColor != null) {
@@ -121,6 +136,7 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
             backgroundColor: backgroundColor,
           );
         }
+
         if (fontColor != null) {
           _passcodeDigitValues[i] = _passcodeDigitValues[i].copyWith(
             fontColor: fontColor,
@@ -143,9 +159,8 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
     }
   }
 
-  void _changeInputMode() => setState(() {
-    _simpleInputMode = !_simpleInputMode;
-  });
+  void _changeInputMode() =>
+      setState(() => _simpleInputMode = !_simpleInputMode);
 
   @override
   Widget build(BuildContext context) {
@@ -185,18 +200,17 @@ class _PasscodeInputViewState extends State<PasscodeInputView>
                 ),
                 const SizedBox(height: 16.0),
                 Expanded(
-                  //to define passcodeinput and rotatory input
                   child:
                       _simpleInputMode
                           ? PasscodeInput(
                             onDigitSelected:
                                 (index) =>
-                                    _onDigitSelected(index, autoValidate: true),
+                                    _onDigitSelected(index, autovalidate: true),
                           )
                           : RotaryDialInput(
-                            pagePadding: _padding,
                             animationDuration: _animationDuration,
                             modeChangeController: _modeChangeController,
+                            pagePadding: _padding,
                             passcodeAnimationInProgress:
                                 _passcodeAnimationInProgress,
                             onDigitSelected: _onDigitSelected,
--- lib/widgets/passcode/passcode_input.dart
+++ lib/widgets/passcode/passcode_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rotatory_passcode/widgets/widgets.dart';
 
 class PasscodeInput extends StatelessWidget {
   final ValueSetter<int> onDigitSelected;
@@ -10,18 +11,20 @@ class PasscodeInput extends StatelessWidget {
     return Column(
       mainAxisAlignment: alignment,
       children: [
-        for(var i = 0; i< 3; i++)
-        Row(
-          mainAxisAlignment: alignment,
-          children: [
-            for(var j = 0; j<3; j++) _renderDialNumber(i*3 + j),
-          ],
-        ),
-        _renderDialNumber(9)
+        for (var i = 0; i < 3; i++)
+          Row(
+            mainAxisAlignment: alignment,
+            children: [
+              for (var j = 0; j < 3; j++) _renderDialNumber(i * 3 + j),
+            ],
+          ),
+        _renderDialNumber(9),
       ],
     );
   }
 
-  Widget _renderDialNumber(int index) =>
-      GestureDetector(onTap: () => onDigitSelected(index), child: Center());
+  Widget _renderDialNumber(int index) => GestureDetector(
+    onTap: () => onDigitSelected(index),
+    child: DialNumber(number: RotaryDialConstants.inputValues[index]),
+  );
 }
